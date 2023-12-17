1979fdd1807d77f159dfe6d3479b152379dabfb6
alpo22 <alan.s.powell@gmail.com>
2023-12-17 15:45:08 -0800

    Update copy

--- src/InstructionsModal/InstructionsModal.jsx
+++ src/InstructionsModal/InstructionsModal.jsx
@@ -9,7 +9,7 @@ export default function InstructionsModal({ isVisible, onClose }) {
       </Modal.Header>
       <Modal.Body>
         <p>A game for Simpsons super-fans!</p>
-        <p>Each day, five quotes from The Simpons are chosen and it is up to you to identify who said each.</p>
+        <p>Each day, five quotes from The Simpons are chosen and it is up to you to identify who said which.</p>
         <p>See how many you can get, and compete with your friends.</p>
       </Modal.Body>
     </Modal>
