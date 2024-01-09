9b50a2af8486e64845f864a8a729ffbd16cc5adf
alpo22 <alan.s.powell@gmail.com>
2024-01-08 23:26:02 -0800

    Get Google font to appear

--- src/app/components/InstructionsModal/InstructionsModal.jsx
+++ src/app/components/InstructionsModal/InstructionsModal.jsx
@@ -8,7 +8,7 @@ export default function InstructionsModal({ isVisible, onClose }) {
         <Modal.Title>Hi Everybody!</Modal.Title>
       </Modal.Header>
       <Modal.Body className="instructions-modal">
-        <p>Welcome to a game for Simpsons super-fans!</p>
+        <p>Welcome to a trivia game for Simpsons super-fans!</p>
         <p>Each day, five quotes from The Simpons are chosen and it is up to you to identify who said each.</p>
         <p>Compete with your friends and see how many you can get.</p>
         <img src={`/assets/characters/Dr_Nick_Riviera.webp`} />
