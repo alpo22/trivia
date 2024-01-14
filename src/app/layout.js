03be00d536f86d3b8ed3553c5b13257130c8d4a3
alpo22 <alan.s.powell@gmail.com>
2024-01-13 22:37:37 -0800

    Update preview text and image

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 export const metadata = {
   title: "AnecDOH!tes",
-  description: "Simpsons quote game",
+  description: "Simpsons daily quote game. Compete against your friends.",
 };
 
 export default function RootLayout({ children }) {
@@ -10,14 +10,14 @@ export default function RootLayout({ children }) {
         <meta property="og:url" content="https://www.anecdohtes.com" />
         <meta property="og:type" content="website" />
         <meta property="og:title" content="AnecDOH!tes" />
-        <meta property="og:description" content="Simpsons quote game" />
+        <meta property="og:description" content="Simpsons daily quote game. Compete against your friends." />
         <meta property="og:image" content="https://www.anecdohtes.com/assets/opengraph.png" />
 
         <meta name="twitter:card" content="summary_large_image" />
         <meta property="twitter:domain" content="anecdohtes.com" />
         <meta property="twitter:url" content="https://www.anecdohtes.com" />
         <meta name="twitter:title" content="AnecDOH!tes" />
-        <meta name="twitter:description" content="Simpsons quote game" />
+        <meta name="twitter:description" content="Simpsons daily quote game. Compete against your friends." />
         <meta name="twitter:image" content="https://www.anecdohtes.com/assets/opengraph.png" />
       </head>
       <body>{children}</body>
