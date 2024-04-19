879462b7245e1d67b91f45b3707e0412fdf3d6f4
alpo22 <alan.s.powell@gmail.com>
2024-04-19 16:48:11 -0700

    Remove comment

--- index.php
+++ index.php
@@ -1,15 +1,6 @@
 <?php
 // To test if this works, go to /api/quotes
 
-// this works:
-// $myDbLink = mysqli_connect(
-//   'db-mysql-test-cluster-do-user-16389943-0.c.db.ondigitalocean.com',
-//   'doadmin',
-//   'AVNS_I5GwOL_5RU1LoeZtvMm',
-//   'defaultdb',
-//   25060
-// ) or die('Database error: ' . mysqli_error($myDbLink));
-
 $pdoConnection = new PDO(
   'mysql:dbname=defaultdb;host=db-mysql-test-cluster-do-user-16389943-0.c.db.ondigitalocean.com;port=25060;charset=utf8',
   'doadmin',
