817abc5527a694488bc2411eab26f7e19331a97d
alpo22 <alan.s.powell@gmail.com>
2025-02-07 12:24:52 -0500

    Add cloud animation

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -1,11 +1,24 @@
 import PropTypes from "prop-types";
 import Heading from "../Heading";
 import "./Intro.scss";
+import "./Clouds.scss";
+import { useEffect } from "react";
 
 export default function Intro({ afterAnimation }) {
+  let audio = new Audio("/assets/angels.mp3");
+
+  useEffect(() => {
+    audio.play(); // it will only play if they interact first; i.e. click a "Play" button splash screen
+  }, []);
+
   return (
     <div className="intro">
-      <Heading onAnimationEnd={afterAnimation} />
+      <div className="intro-heading-wrapper">
+        <Heading onAnimationEnd={afterAnimation} />
+      </div>
+      <div className="cloud cloud1" />
+      <div className="cloud cloud2" />
+      <div className="cloud cloud3" />
     </div>
   );
 }
